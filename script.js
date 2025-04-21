677e5783b86447fa18b4d07f7a981ad3a4af8c3e
Taseer Baig <taseer.baig@emumba.com>
2025-04-21 22:27:54 +0500

    [ impl ] - landing page implementation

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.btn-book').forEach(button => {
+  button.addEventListener('click', () => {
+      alert("Table booking feature is under development. Stay tuned!");
+  });
+});
